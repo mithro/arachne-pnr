a256c935cac672e5aa6289ba437553e2a01fbec4
Tim 'mithro' Ansell <me@mith.ro>
2018-06-05 08:37:36 -0700

    Remove trailing white space.

--- src/global.cc
+++ src/global.cc
@@ -1,15 +1,15 @@
 /* Copyright (C) 2015 Cotton Seed
-   
+
    This file is part of arachne-pnr.  Arachne-pnr is free software;
    you can redistribute it and/or modify it under the terms of the GNU
    General Public License version 2 as published by the Free Software
    Foundation.
-   
+
    This program is distributed in the hope that it will be useful, but
    WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program. If not, see <http://www.gnu.org/licenses/>. */
 
@@ -28,24 +28,24 @@ class Promoter
 {
   std::vector<uint8_t> global_classes;
   static const char *global_class_name(uint8_t gc);
-  
+
   DesignState &ds;
   const ChipDB *chipdb;
   Design *d;
   Model *top;
   const Models &models;
   std::map<Instance *, uint8_t, IdLess> &gb_inst_gc;
-  
+
   Net *const0;
-  
+
   uint8_t port_gc(Port *conn, bool indirect);
   void pll_pass_through(Instance *inst, int cell, const char *p_name);
   bool routable(int g, Port *p);
   void make_routable(Net *n, int g);
-  
+
 public:
   Promoter(DesignState &ds_);
-  
+
   void promote(bool do_promote);
 };
 
@@ -86,7 +86,7 @@ Promoter::Promoter(DesignState &ds_)
           break;
         }
     }
-  
+
   // will prune
   if (!const0)
     {
@@ -101,7 +101,7 @@ Promoter::port_gc(Port *conn, bool indirect)
 {
   Instance *inst = dyn_cast<Instance>(conn->node());
   assert(inst);
-  
+
   if (models.is_lc(inst))
     {
       if (conn->name() == "CLK")
@@ -177,7 +177,7 @@ Promoter::port_gc(Port *conn, bool indirect)
       else if (conn->name() == "RE")
         return gc_re;
     }
-  
+
   return 0;
 }
 
@@ -194,10 +194,10 @@ Promoter::pll_pass_through(Instance *inst, int cell, const char *p_name)
   Net *n = p->connection();
   if (!n)
     return;
-  
+
   Net *t = top->add_net(n);
   p->connect(t);
-  
+
   Instance *pass_inst = top->add_instance(models.lc);
   pass_inst->find_port("I0")->connect(t);
   pass_inst->find_port("I1")->connect(const0);
@@ -205,10 +205,10 @@ Promoter::pll_pass_through(Instance *inst, int cell, const char *p_name)
   pass_inst->find_port("I3")->connect(const0);
   pass_inst->set_param("LUT_INIT", BitVector(2, 2));
   pass_inst->find_port("O")->connect(n);
-  
+
   const auto &p2 = chipdb->cell_mfvs.at(cell).at(p_name);
   int pass_cell = chipdb->loc_cell(Location(p2.first, 0));
-  
+
   extend(ds.placement, pass_inst, pass_cell);
 }
 
@@ -221,16 +221,16 @@ Promoter::make_routable(Net *n, int gc)
     {
       Port *p = *i;
       ++i;
-      
+
       if (!p->is_input())
         continue;
       if (routable(gc, p))
         continue;
-      
+
       if (!internal)
         {
           internal = top->add_net(n);
-          
+
           Instance *pass_inst = top->add_instance(models.lc);
           pass_inst->find_port("I0")->connect(n);
           pass_inst->find_port("I1")->connect(const0);
@@ -250,9 +250,9 @@ Promoter::promote(bool do_promote)
   std::map<Net *, int, IdLess> net_idx;
   std::tie(nets, net_idx) = top->index_nets();
   int n_nets = nets.size();
-  
+
   int n_global = 0;
-  
+
   std::map<uint8_t, int> gc_global;
   std::map<uint8_t, int> gc_used;
   for (uint8_t gc : global_classes)
@@ -260,13 +260,13 @@ Promoter::promote(bool do_promote)
       extend(gc_global, gc, 0);
       extend(gc_used, gc, 0);
     }
-  
+
   std::vector<std::pair<Instance *, int>> plls;
   for (const auto &p : ds.placement)
     {
       Instance *inst = p.first;
       int c = p.second;
-      
+
       if (models.is_gb_io(inst))
         {
           Port *out = inst->find_port("GLOBAL_BUFFER_OUTPUT");
@@ -304,7 +304,7 @@ Promoter::promote(bool do_promote)
           if (out->connected() && !inst->is_attr_set("ROUTE_THROUGH_FABRIC"))
             {
               int driven_glb = chipdb->get_oscillator_glb(c, "CLKLF");
-              
+
               for (uint8_t gc : global_classes)
                 {
                   if (gc & (1 << driven_glb))
@@ -316,7 +316,7 @@ Promoter::promote(bool do_promote)
       else if (models.is_pllX(inst))
         {
           plls.push_back(std::make_pair(inst, c));
-          
+
           Port *a = inst->find_port("PLLOUTGLOBAL");
           if (!a)
             a = inst->find_port("PLLOUTGLOBALA");
@@ -333,7 +333,7 @@ Promoter::promote(bool do_promote)
                 }
               make_routable(a->connection(), 1 << g);
             }
-          
+
           Port *b = inst->find_port("PLLOUTGLOBALB");
           if (b && b->connected())
             {
@@ -349,7 +349,7 @@ Promoter::promote(bool do_promote)
             }
         }
     }
-  
+
   for (const auto &p : plls)
     {
       Instance *inst = p.first;
@@ -357,9 +357,9 @@ Promoter::promote(bool do_promote)
       pll_pass_through(inst, c, "LOCK");
       pll_pass_through(inst, c, "SDO");
     }
-  
+
   std::set<Net *, IdLess> boundary_nets = top->boundary_nets(d);
-  
+
   std::set<std::pair<int, int>, std::greater<std::pair<int, int>>> promote_q;
   std::map<int, uint8_t> net_gc;
   std::map<int, Port *> net_driver;
@@ -369,11 +369,11 @@ Promoter::promote(bool do_promote)
       if (contains(boundary_nets, n)
           || n->is_constant())
         continue;
-      
+
       std::map<uint8_t, int> n_gc;
       for (uint8_t gc : global_classes)
         extend(n_gc, gc, 0);
-      
+
       Port *driver = nullptr;
       for (Port *conn : n->connections())
         {
@@ -383,12 +383,12 @@ Promoter::promote(bool do_promote)
               assert(!driver);
               driver = conn;
             }
-          
+
           int gc = port_gc(conn, false);
           if (gc)
             ++n_gc[gc];
         }
-      
+
       int max_gc = 0;
       int max_n = 0;
       for (const auto &p : n_gc)
@@ -399,7 +399,7 @@ Promoter::promote(bool do_promote)
               max_n = p.second;
             }
         }
-      
+
       if (driver
           && isa<Instance>(driver->node())
           && ((models.is_gbX(cast<Instance>(driver->node()))
@@ -408,27 +408,27 @@ Promoter::promote(bool do_promote)
                   && (driver->name() == "PLLOUTGLOBAL"
                       || driver->name() == "PLLOUTGLOBALA"
                       || driver->name() == "PLLOUTGLOBALB"))
-              || (models.is_hfosc(cast<Instance>(driver->node())) 
-                    && driver->name() == "CLKHF" && 
+              || (models.is_hfosc(cast<Instance>(driver->node()))
+                    && driver->name() == "CLKHF" &&
                         !cast<Instance>(driver->node())->is_attr_set("ROUTE_THROUGH_FABRIC"))
-              || (models.is_lfosc(cast<Instance>(driver->node())) 
-                    && driver->name() == "CLKLF" && 
+              || (models.is_lfosc(cast<Instance>(driver->node()))
+                    && driver->name() == "CLKLF" &&
                         !cast<Instance>(driver->node())->is_attr_set("ROUTE_THROUGH_FABRIC"))))
         {
           Instance *gb_inst = cast<Instance>(driver->node());
-          
+
           uint8_t gc = max_gc ? max_gc : gc_clk;
-          
+
           ++n_global;
           ++gc_global[gc];
-          
-          if (models.is_gbX(gb_inst) || 
+
+          if (models.is_gbX(gb_inst) ||
               models.is_hfosc(gb_inst) ||
               models.is_lfosc(gb_inst))
             {
               if (driver->connected())
                 make_routable(driver->connection(), gc);
-              
+
               extend(gb_inst_gc, gb_inst, gc);
             }
           for (uint8_t gc2 : global_classes)
@@ -447,22 +447,22 @@ Promoter::promote(bool do_promote)
           promote_q.insert(std::make_pair(max_n, i));
         }
     }
-  
+
   int n_promoted = 0;
   std::map<uint8_t, int> gc_promoted;
   for (int gc : global_classes)
     extend(gc_promoted, gc, 0);
-  
+
   while(!promote_q.empty())
     {
       std::pair<int, int> p = *promote_q.begin();
       promote_q.erase(promote_q.begin());
       assert(promote_q.empty()
              || promote_q.begin()->first <= p.first);
-      
+
       Net *n = nets[p.second];
       uint8_t gc = net_gc.at(p.second);
-      
+
       for (int gc2 : global_classes)
         {
           int k2 = 0;
@@ -471,21 +471,21 @@ Promoter::promote(bool do_promote)
               if (gc2 & (1 << i))
                 ++k2;
             }
-          
+
           if ((gc2 & gc) == gc)
             {
               if (gc_used.at(gc2) >= k2)
                 goto L;
             }
         }
-      
+
       {
         ++n_promoted;
         ++gc_promoted[gc];
-        
+
         Instance *gb_inst = top->add_instance(models.gb);
         Net *t = top->add_net(n);
-        
+
         int n_conn = 0;
         int n_conn_promoted = 0;
         for (auto i = n->connections().begin();
@@ -496,7 +496,7 @@ Promoter::promote(bool do_promote)
             if (conn->is_output()
                 || conn->is_bidir())
               continue;
-            
+
             ++n_conn;
             int conn_gc = port_gc(conn, true);
             if ((conn_gc & gc) == gc)
@@ -505,10 +505,10 @@ Promoter::promote(bool do_promote)
                 conn->connect(t);
               }
           }
-        
+
         gb_inst->find_port("USER_SIGNAL_TO_GLOBAL_BUFFER")->connect(n);
         gb_inst->find_port("GLOBAL_BUFFER_OUTPUT")->connect(t);
-        
+
         ++n_global;
         ++gc_global[gc];
         extend(gb_inst_gc, gb_inst, gc);
@@ -522,7 +522,7 @@ Promoter::promote(bool do_promote)
       }
     L:;
     }
-  
+
   *logs << "  promoted " << n_promoted << " nets\n";
   for (const auto &p : gc_promoted)
     {
@@ -535,7 +535,7 @@ Promoter::promote(bool do_promote)
       if (p.second)
         *logs << "    " << p.second << " " << global_class_name(p.first) << "\n";
     }
-  
+
   d->prune();
 }
 
